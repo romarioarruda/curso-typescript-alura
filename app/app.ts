aef296837b6671c058af4706d5d289ae91323495
Romário Arruda <romarioarruda98@gmail.com>
2022-04-01 11:17:18 -0300

    validando erro de identificacao no form

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,10 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
 
 const controller = new NegociacaoController();
-const form = document.querySelector('.form') as Element;
+const form = document.querySelector('.form');
+
+if(!form) throw new Error('Formulário não foi reconhecido.')
+
 form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
