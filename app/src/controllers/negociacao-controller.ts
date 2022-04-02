2ba2fc776e9b9c2392ffc992491da87b9ab4dd0f
Romário Arruda <romarioarruda98@gmail.com>
2022-04-02 10:38:33 -0300

    interface para api negociacoes

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -1,6 +1,7 @@
 import { domInjector } from '../decorators/dom-injector.js';
 import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { NegociacoesDoDia } from '../interfaces/negociacoes-do-dia.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
@@ -41,6 +42,23 @@ export class NegociacaoController {
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+    }
+
+    public importarDados(): void {
+        fetch('http://localhost:8080/dados')
+            .then(resp => resp.json())
+            .then((dados: NegociacoesDoDia[]) => {
+                return dados.map(dadosDeHoje => {
+                    return new Negociacao(new Date(), dadosDeHoje.vezes, dadosDeHoje.montante)
+                })
+            })
+            .then((negociacoesHoje: Negociacao[]) => {
+                for(let negociacao of negociacoesHoje) {
+                    this.negociacoes.adiciona(negociacao)
+                }
+
+                this.negociacoesView.update(this.negociacoes)
+            })
     }
 
     private ehDiaUtil(data: Date) {
